dd01a9f01f7ac8d9ef80fe607da709e34bb635a7
Samuel Chávez <schavezfuentes@gmail.com>
2021-01-28 17:17:55 -0600

    Propiedad de multiplicacion matricial

--- clase3/tests.py
+++ clase3/tests.py
@@ -1,13 +1,35 @@
 import numpy as np
 
-print(type(np.arange(10, 20, 2)))
+# print(type(np.arange(10, 20, 2)))
 
-a = np.arange(1, 10).reshape((3, 3))
-b = np.arange(10, 13).reshape((3, 1))
+# a = np.arange(1, 10).reshape((3, 3))
+# b = np.arange(10, 13).reshape((3, 1))
 
-print(a)
-print(b)
-print(a.shape)
-print(b.shape)
+# print(a)
+# print(b)
+# print(a.shape)
+# print(b.shape)
 
-print(b.T @ a)
+# print(b.T @ a)
+
+# 5 + 6 * 1 + 7 * 2
+# 5 + 6 * 3 + 7 * 4
+
+X = np.array([
+    [1, 3],
+    [2, 4],
+])
+
+X = np.vstack((
+    np.ones(2).reshape(1, 2),
+    X
+))
+
+Theta = np.array([
+    [5],
+    [6],
+    [7],
+])
+
+print(X.T @ Theta)
+
